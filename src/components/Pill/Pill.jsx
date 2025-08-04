52f1a562ada0ac02bc772aee6e33e0768762b45a
Phanindra <tphanindra28@gmail.com>
2025-08-04 11:10:52 +0530

    Add Deploy scripts

--- src/components/Pill/Pill.jsx
+++ src/components/Pill/Pill.jsx
@@ -2,7 +2,7 @@ import "./Pill.css";
 export default function Pill({ status }) {
   return (
     <span
-      className={`border p-1 pill ${status === "paid" ? "pill-green" : ""}`}
+      className={`border p-1 px-2 pill ${status === "paid" ? "pill-green" : ""}`}
     >
       {status}
     </span>
